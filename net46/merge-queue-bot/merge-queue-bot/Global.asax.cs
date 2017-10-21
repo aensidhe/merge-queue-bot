f4a8bcf7f4c34303752852b5a12ca94d5a7561c3
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-10-21 16:03:45 +0300

    Hello world for bot is working ok.

---
+++ net46/merge-queue-bot/merge-queue-bot/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace AenSidhe.MergeQueueBot
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
